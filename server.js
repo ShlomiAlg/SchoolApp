84435d9de3f683f42655d27304f0497d7d0d0c93
ShlomiAlg <algshlomi@gmail.com>
2026-06-01 21:43:49 +0000

    02

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.send('Server is running 🚀');
 });
