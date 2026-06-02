16effa14c07a610cf096dbe3f50f2e270e6967c4
ShlomiAlg <algshlomi@gmail.com>
2026-06-02 06:59:38 +0000

    21

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
   cors: { origin: "*" }
 });
 io.on('connection', (socket) => {
-  console.log('Client connected');
+  console.log('Client connected:', socket.id);
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
